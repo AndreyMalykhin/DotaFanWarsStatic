0a06ddee440fa9290329d500873fcfc097b5064d
AndreyMalykhin <shell100500@gmail.com>
2016-07-30 19:35:56 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ envConfigFile="$1"
 
 mv .env .env_
 cp "${envConfigFile}" .env
-vagrant up --provider digital_ocean
+vagrant up --no-provision --provider digital_ocean
+vagrant provision
 rm .env
 mv .env_ .env
